49006008b72907badc13ec3b59e73da3508a6343
dxy <dxy@qq.com>
2024-03-14 21:01:21 +0800

    second commit

--- src/main/java/com/atuguigu/git.java
+++ src/main/java/com/atuguigu/git.java
@@ -7,5 +7,7 @@ package com.atuguigu;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
+
     }
 }
